710368fb2754093e2cc22936633da3cddc4ac1c9
nickphoon <potatonicholas@gmail.com>
2024-07-21 20:18:03 +0800

    update

--- flask/test_selenium.py
+++ flask/test_selenium.py
@@ -9,10 +9,10 @@ class FlaskAppTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         options = Options()
-        options.headless = True
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-gpu')
+        options.add_argument("--headless")  # Run in headless mode
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--remote-debugging-port=9222")
         options.add_argument('--remote-debugging-port=9222')
         cls.driver = webdriver.Chrome(options=options)
         cls.driver.implicitly_wait(10)
